972d1048c68a1147c1b520a6a165f7de6405c3b7
phennaux <phennaux115@gmail.com>
2022-01-08 21:10:42 +0100

    Treewide: Massive fixes and some cleaning Fix compilation issues after rebase Add storm_defconfig Remove product mounting and disable avb Add missing firmware files Nuke securities from Samsung and avoid building them Add sar_ramdisk to boot in AOSP ROMs

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,19 +1,52 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+KERNEL_DEFCONFIG=storm_defconfig
 
-mkdir out
+echo
+echo "==> Clean Build Directory"
+echo 
 
-BUILD_CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
-CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+mkdir -p out
+make O=out clean
 
-#export ARCH=arm64
-#make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android a70q_eur_open_defconfig
-#make -j64 -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android
+echo
+echo "==> Issue Build Commands"
+echo
 
-make -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE a70q_eur_open_defconfig
-make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+export HOME=/home/$USER
+export ARCH=arm64
+export SUBARCH=arm64
+export CLANG_PATH=$HOME/proton-clang/bin
+export PATH="$CLANG_PATH:$PATH"
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+export KBUILD_BUILD_USER=rundi
+export KBUILD_BUILD_HOST=a70q
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+START=$(date +"%s")
+
+echo
+echo "==> Kernel is using the current defconfig: $KERNEL_DEFCONFIG"
+echo
+ 
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out $KERNEL_DEFCONFIG
+
+echo
+echo "==> Begin compilation..."
+echo 
+
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out -j$(nproc --all)
+
+echo
+echo "==> Verify Image.gz.dtb and dtbo.img..."
+echo
+
+ls $PWD/out/arch/arm64/boot/Image.gz-dtb
+ls $PWD/out/arch/arm64/boot/dtbo.img
+
+END=$(date +"%s")
+DIFF=$((END - START))
+
+echo
+echo "==> Kernel compiled successfully in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+echo
